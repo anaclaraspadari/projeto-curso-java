b3a8135349087681f78383ddbb538bc57e8bdffe
Ana Clara Spadari <anaclaracdesspadari@gmail.com>
2024-02-12 19:12:14 -0300

    Creating class Seller

--- src/application/App.java
+++ src/application/App.java
@@ -1,46 +1,24 @@
 package application;
 
 import java.sql.Connection;
+import java.util.Date;
 import java.util.Scanner;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
 
 	public static void main(String[] args) {
 		
         Department obj=new Department(1,"Books");
-        System.out.println(obj);
 
-		// Scanner sc=new Scanner(System.in);
-		// Connection conn=null;
-		
+        Seller seller=new Seller(1,"Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
-		// System.out.println("Nome:");
-		// String name=sc.nextLine();
-		
-		// sc.close();
-		
-		// PreparedStatement st=null;
-		
-		// try {
-		// 	conn=DB.getConnection();
-			
-		// 	st=conn.prepareStatement("");
-		// 	st.setString(1,name);
-		// 	int rowsAffected=st.executeUpdate();
-
-		// 	System.out.println("DONE! Rows affected: "+rowsAffected);
-			
-		// }catch(SQLException e) {
-		// 	e.printStackTrace();
-		// }finally {
-		// 	DB.closeStatement(st);
-		// 	DB.closeConnection();
-		// }
+        System.out.println(seller);
 		
 	}
 
