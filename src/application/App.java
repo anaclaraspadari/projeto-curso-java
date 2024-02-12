a3ffae9fd1740f18db9e1b46b6824e6c7115f9ad
Ana Clara Spadari <anaclaracdesspadari@gmail.com>
2024-02-12 19:03:03 -0300

    Creating class Department

--- src/application/App.java
+++ src/application/App.java
@@ -1,43 +1,46 @@
 package application;
 
 import java.sql.Connection;
-
 import java.util.Scanner;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import model.entities.Department;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		Scanner sc=new Scanner(System.in);
-		Connection conn=null;
+        Department obj=new Department(1,"Books");
+        System.out.println(obj);
+
+		// Scanner sc=new Scanner(System.in);
+		// Connection conn=null;
 		
 
-		System.out.println("Nome:");
-		String name=sc.nextLine();
+		// System.out.println("Nome:");
+		// String name=sc.nextLine();
 		
-		sc.close();
+		// sc.close();
 		
-		PreparedStatement st=null;
+		// PreparedStatement st=null;
 		
-		try {
-			conn=DB.getConnection();
+		// try {
+		// 	conn=DB.getConnection();
 			
-			st=conn.prepareStatement("");
-			st.setString(1,name);
-			int rowsAffected=st.executeUpdate();
+		// 	st=conn.prepareStatement("");
+		// 	st.setString(1,name);
+		// 	int rowsAffected=st.executeUpdate();
 
-			System.out.println("DONE! Rows affected: "+rowsAffected);
+		// 	System.out.println("DONE! Rows affected: "+rowsAffected);
 			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		// }catch(SQLException e) {
+		// 	e.printStackTrace();
+		// }finally {
+		// 	DB.closeStatement(st);
+		// 	DB.closeConnection();
+		// }
 		
 	}
 
